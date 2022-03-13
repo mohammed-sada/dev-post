d767e813f349099b699a7ae7e7d2778e160946ca
mohammed-sada <mohammedabusada94@example.com>
2022-03-13 14:15:40 +0200

    fix user's name in profile page

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -3,15 +3,17 @@ import React from 'react';
 
 export default function UserProfile({ user }) {
   return (
-    <div className='pt-10 flex flex-col justify-center items-center '>
+    <div className='text-center pt-10 flex flex-col justify-center items-center '>
       <Image
         src={user.photoURL || '/static/photo.png'}
         alt={user.displayName}
         height='100'
         width='100'
       />
-      <p className='mt-2 text-xl italic'>@{user.username}</p>
-      <h1 className='mt-8 text-4xl font-bold capitalize'>{user.displayName}</h1>
+      <p className=' mt-2 text-xl italic'>@{user.username}</p>
+      <h1 className='mt-8 text-3xl lg:text-4xl font-bold capitalize'>
+        {user.displayName}
+      </h1>
     </div>
   );
 }
